10e58c41ccc79ce292f25a5f170c0fe97b5328fd
GABRIEL DE ASSIS SPERANDIO <g.sperandio@labcetec.net>
2024-09-16 17:12:15 -0300

    alteracao

--- client/src/components/tabelaAulas/AbreviaUc.jsx
+++ client/src/components/tabelaAulas/AbreviaUc.jsx
@@ -1,7 +1,7 @@
 function AbreviaUc(props) {
     const materia = (props.materia);
     const unidade = materia.split(' ');
-    const uc = (unidade[0].subtring(0,4) + ' ' + unidade[unidade.lenght-3]);
+    const uc = (unidade[0].substring(0,4) + ' ' + unidade[unidade.lenght-3]);
   
     return (uc);
 }
